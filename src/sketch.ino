ffeba82c31dac3993039f7a60951edd8dc7581c3
NicolePT <U201921559@upc.edu.pe>
2023-11-16 18:26:59 -0500

    feat: add authentication on setup

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,19 +1,14 @@
+#include "Arduino.h"
 #include "wifi_utils.h"
 #include "weather_sensor.h"
 #include "http_utils.h"
-#define DELAY 3000
-const char* ssid = "Wokwi-GUEST";
-const char* password = "";
-const char* serverAddress = "https://lifetravel-iot-backend.azurewebsites.net/api/v1/weather-sensors/update-weather/1";
-const char* authToken = "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImQ0OWU0N2ZiZGQ0ZWUyNDE0Nzk2ZDhlMDhjZWY2YjU1ZDA3MDRlNGQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vbGlmZXRyYXZlbC1hcHAiLCJhdWQiOiJsaWZldHJhdmVsLWFwcCIsImF1dGhfdGltZSI6MTY5OTM3NDk2OSwidXNlcl9pZCI6IjJpRmNqeTJVaHhOdkJET1pNclFkMlZEb0lzeTIiLCJzdWIiOiIyaUZjankyVWh4TnZCRE9aTXJRZDJWRG9Jc3kyIiwiaWF0IjoxNjk5Mzc0OTY5LCJleHAiOjE2OTkzNzg1NjksImVtYWlsIjoiaW90QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJpb3RAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.IvrGpcMFrBCgQkWGsDSfBR99DrI3U66l5y89KAgFkiHVQjuPCIUUP2rG5Y40smOBx9VmZGaDOPpEcJiVoQR7aH7AJcH3K8kIienC9cxVhsp3xPYEOJ6_yNK34vi8bP8n7tsOQIAk65a-xrFR2EtnNe2318DAqrsBFu0ArKpnzw597-zjRj3o2tIoTrvzNW1IbKuTjKfUsgiKoy6orMnB944gP5IN2y2T7lHZZ40N5DYn4VeDkS37t6pY5aVWLfg8dDT0Hr_dInxRIll2JuGohLt6bHW1-bBkfy4e1VBoB5JuNy1R9gglCruda2XMIhDMA53MV4NuI4zFsWePh08U6Q";
-// please update the token here
-// https://iot-lifetravel-firebase-token-gen.netlify.app
-// email: iot@gmail.com
-// password: 12345678
-
+#define DELAY 500
+String authToken;
 void setup() {
   Serial.begin(115200);
-  setupWiFi(ssid, password);
+  setupWiFi();
+  //authToken = authAndGetToken();
+  authToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImE2YzYzNTNmMmEzZWMxMjg2NTA1MzBkMTVmNmM0Y2Y0NTcxYTQ1NTciLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vbGlmZXRyYXZlbC1hcHAiLCJhdWQiOiJsaWZldHJhdmVsLWFwcCIsImF1dGhfdGltZSI6MTcwMDE3NTU1NiwidXNlcl9pZCI6IjJpRmNqeTJVaHhOdkJET1pNclFkMlZEb0lzeTIiLCJzdWIiOiIyaUZjankyVWh4TnZCRE9aTXJRZDJWRG9Jc3kyIiwiaWF0IjoxNzAwMTc1NTU2LCJleHAiOjE3MDAxNzkxNTYsImVtYWlsIjoiaW90QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJpb3RAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.X4AWzFcKFRhiX0Q40w03T90TdjWqBAtgGXHglveHPnqAVcLw7DD2C4P2kWRsujSF0us0pPWtEpO9Q7HO8nCcz-rkEimvgKcZtjhTIZJ4fDbKsDVIbiVNF8yLge8O7DBEueoKIbUtoGiH0rJzjlN0C0mJ2w1HDJv3C7_6PvIvdi5gXQUJbZH05cAbMCuH4PU7pBMZDCHFh-E8wz5_m54o6s4TxNEJz-dmVJ7gi8QS-uMzlwKVDAkDFu_9VtJ8ICDFEpAbQaGjW0dBT8DkFlaF7i_fIva7Ff3ecMvQCsv0DohRp5exASf2evn4l2e3xfm_GERFuWEsM8pQmDB6M3Q89Q";
   setupWeatherSensor();
 }
 
@@ -24,17 +19,10 @@ void loop() {
   if (!isnan(temperature) && !isnan(humidity)) {
     String requestBody = String("{\"temperature\":") + temperature + ",\"humidity\":" + humidity + "}";
     Serial.println(requestBody);
-    int httpResponseCode = sendPUTRequest(serverAddress, requestBody.c_str(), authToken);
-
-    if (httpResponseCode == 200) {
-      Serial.println("Actualizado con éxito en el servidor.");
-    } else {
-      Serial.print("Error en la solicitud. Código de respuesta HTTP: ");
-      Serial.println(httpResponseCode);
-    }
+    int httpResponseCode = sendPUTRequest(requestBody.c_str(), authToken.c_str());
+    checkResponseCode(httpResponseCode);
   } else {
     Serial.println("Error al leer los datos del sensor DHT.");
   }
-
   delay(DELAY);
 }
