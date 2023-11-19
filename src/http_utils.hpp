c518c47e9a97e0abc639d449dd3d8e1cddde2d99
NicolePT <U20201B980@upc.edu.pe>
2023-11-19 12:38:09 -0500

    chore: fix convention

--- src/http_utils.hpp
+++ src/http_utils.hpp
@@ -1,6 +1,6 @@
 #pragma once
-#ifndef HTTP_UTILS_H
-#define HTTP_UTILS_H
+#ifndef HTTP_UTILS_HPP
+#define HTTP_UTILS_HPP
 
 const char* authAndGetToken(const char* email = "iot@gmail.com", const char* password = "12345678");
 int sendPUTRequest(const char* requestBody, const char* authToken, int resourceId = 1);
